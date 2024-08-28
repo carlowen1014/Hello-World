8a171a187d6f843fe78701021ffbeafc9bcebd02
carlowen1014 <cowen8@cub.uca.edu>
2024-08-28 14:39:19 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//Carl Owen
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        // Print Hello, World! statement
+        System.out.println("Hello, World!");
+    }
+}
